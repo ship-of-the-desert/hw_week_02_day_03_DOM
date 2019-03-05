1e3a26e4c5b24c29be0f3f006a387e38333b7fe8
muneraabdul96@hotmail.com <munera@mouneras-MacBook-Pro.local>
2019-03-05 21:18:52 +0300

    /Users/munera/desktop/w2d3/hw_week_02_day_03_DOM/

---
+++ dom.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    document.getElementById("myp").style.fontFamily = "Arial, sans-serif"; 
+    document.getElementById("nickname").style.fontFamily = "Arial, sans-serif"; 
+    document.getElementById("myp").style.textAlign = "center";
+    document.getElementById("nickname").style.textAlign = "center";
+    document.getElementById("nickname").textContent="Muner Alhazza";
+    document.getElementById("favorite").textContent="Cat";
+    document.getElementById("hometown").textContent="Riyadh";
+    
+}
+myFunction() ;
