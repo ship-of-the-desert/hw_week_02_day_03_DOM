3c91f8d5e9cae7f9b60345302d26a1c9dc0b9d77
rehabsaadoun <YOUR GITHUB EMAIL>
2019-03-17 16:06:28 +0300

    hw w02d03

---
+++ domh.js
@@ -0,0 +1,75 @@
+
+
+
+var me =
+ document.body.style.fontFamily = 'Arial, sans-serif';
+ document.getElementById('nickname').textContent = 'Robe.';
+ document.getElementById('favorites').textContent = 'Education Design,Travel.';
+ document.getElementById('hometown').textContent = 'Riyadh.';
+ 
+ var liItems = document.getElementsByTagName('li'),
+     numItems,
+     i = 0;
+ numItems = liItems.length;
+ for (i; i < numItems; i += 1) { 
+     liItems[i].className = 'listitem';
+ }
+
+ imgElem = document.createElement('img');
+ imgElem.src = 'https://m.media-amazon.com/images/I/91vMemHo8CL._AC_UL654_FMwebp_QL65_.jpg';
+ document.body.appendChild(imgElem);
+
+
+ 
+  document.write<h1>"My Bok List"<h1>
+
+
+var books =
+  
+      title= "The Design of Everyday Things",
+      author= "Don Norman",
+      alreadyRead= false
+      imgElem = document.createElement('img'),
+ imgElem.src = 'https://m.media-amazon.com/images/I/81zpLhP1gWL._AC_UL654_FMwebp_QL65_.jpg';
+ document.body.appendChild(imgElem),
+    
+      
+      
+    
+      title="The Most Human Human",
+      author="Brian Christian",
+      alreadyRead= true
+
+      imgElem = document.createElement('img');
+ imgElem.src = 'https://m.media-amazon.com/images/I/71HMyqG6MRL._AC_UL654_FMwebp_QL65_.jpg';
+ document.body.appendChild(imgElem);
+    
+
+
+    
+      title= "In Search of Lost Time",
+      author="Marcel Proust",
+      alreadyRead= true
+      imgElem = document.createElement('img');
+ imgElem.src = 'https://m.media-amazon.com/images/I/71CzIhvPeIL._AC_UL654_FMwebp_QL65_.jpg';
+ document.body.appendChild(imgElem);
+    
+    
+      title= "Ulysses",
+      author= "James Joyce",
+      alreadyRead= true
+      imgElem = document.createElement('img');
+ imgElem.src = 'https://m.media-amazon.com/images/I/31d8ajQ0wDL._AC_UL654_FMwebp_QL65_.jpg';
+ document.body.appendChild(imgElem);
+    
+    
+      title= "The Great Gatsby",
+      author= "F. Scott Fitzgerald",
+      alreadyRead= true
+      imgElem = document.createElement('img');
+ imgElem.src = 'https://m.media-amazon.com/images/I/91hQ0qlofEL._AC_UL654_FMwebp_QL65_.jpg';
+ document.body.appendChild(imgElem);
+    
+
+
+  
